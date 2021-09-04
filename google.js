bfa05733ddb1018c50815326d1aee32470fd5553
brenofelixdasilva <85354255+brenofelixdasilva@users.noreply.github.com>
2021-09-03 21:52:36 -0300

    Adição dos botões de pesquisa e imagem arredondada

---
+++ google.js
@@ -0,0 +1,12 @@
+const busca = document.getElementById("caixa");
+const pesquisar = document.getElementById("pesquisar");
+const resultados = document.getElementById("result");
+
+let contador = 0;
+
+pesquisar.addEventListener("click",function(){
+    const fraseDigitada = caixa.value;
+    result.innetHTML ="Você pesquisou por: " + fraseDigitada + "!";
+    });
+
+    alert("desconjuração")
